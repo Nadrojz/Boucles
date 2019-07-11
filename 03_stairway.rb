83bf4a12a4939e91c6bf74b78efa52623b292704
Nadrojz <jor.perrin@gmail.com>
2019-07-11 18:52:33 +0200

    Exercices finished

--- 03_stairway.rb
+++ 03_stairway.rb
@@ -1,27 +1,68 @@
+# Renvoie un chiffre entre 1 et 6
 def randomNumberMet
   rand(1..6)
 end
 
-stair = 0
-
-while stair >= 0 && stair <= 9
-  randomNumber=randomNumberMet
-  if randomNumber >= 5
-    puts "Tu as fait le chiffre #{randomNumber}, tu montes d'une marche"
-    stair = stair + 1
-    puts "Tu es maintenant sur la marche numéro #{stair}"
-  elsif randomNumber < 5 && randomNumber > 1
-    puts "Tu as fait le chiffre #{randomNumber}, tu restes sur la marche numéro #{stair}"
-    stair = stair
-  elsif randomNumber == 1 && stair != 0
-    puts "Tu as fait le chiffre #{randomNumber}, tu descends d'une marche"
-    stair = stair - 1
-    puts "Tu es maintenant sur la marche numéro #{stair}"
-  elsif randomNumber == 1 && stair == 0
-    stair = 0
-    puts "Tu restes en bas de l'escalier"
-  end
-  if stair == 10
-    puts "Bravo, tu as gagné"
-  end
+def message_up stairup
+  "Tu montes d'un étage, tu es maintenant à l'étage #{stairup}"
 end
+
+def message_down stairdown
+  "Tu descends d'un étage, tu es maintenant à l'étage #{stairdown}"
+end
+
+def message_stay stairstay
+  "Tu restes au même étage, tu es toujours à l'étage #{stairstay}"
+end
+
+def message_win
+  "Bravo, tu as gagné !"
+end
+
+def message_stairmin stairmin
+  "Tu es sur la marche #{stairmin}, tu ne peux pas descendre plus bas"
+end
+
+
+def moveUser
+  stair = 0
+  i = 1
+  while stair >= 0 && stair <= 9
+    randomNumber = randomNumberMet
+    puts "-----------------------"
+    puts "Début du tour numéro #{i}"
+    puts "-----------------------"
+    if randomNumber >= 5
+      puts "Le dé est lancé ! Tu as fait le chiffre #{randomNumber}"
+      stair += 1
+      puts message_up stair
+    elsif randomNumber < 5 && randomNumber > 1
+      puts "Le dé est lancé ! Tu as fait le chiffre #{randomNumber}"
+      stair = stair
+      puts message_stay stair
+    elsif randomNumber == 1 && stair != 0
+      puts "Le dé est lancé ! Tu as fait le chiffre #{randomNumber}"
+      stair -= 1
+      puts message_down stair
+    elsif randomNumber == 1 && stair == 0
+      puts "Le dé est lancé ! Tu as fait le chiffre #{randomNumber}"
+      stair = 0
+      puts message_stairmin stair
+    end
+    if stair == 10
+      puts message_win
+    end
+  puts "-----------------------"
+  puts "Fin du tour numéro #{i}"
+  puts "-----------------------"
+  i +=1
+  end
+  return stair
+end
+
+
+def perform
+  moveUser
+end
+
+ perform
